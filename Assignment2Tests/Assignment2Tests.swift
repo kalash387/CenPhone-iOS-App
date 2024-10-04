86fa8e624d0c8e9090efa78f20a546635f7f685f
kalash199663 <krami2@my.centennialcollege.ca>
2024-10-03 23:16:55 -0400

    Initial Commit

---
+++ Assignment2Tests/Assignment2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment2Tests.swift
+//  Assignment2Tests
+//
+//  Created by Kalash on 2024-10-03.
+//
+
+import Testing
+@testable import Assignment2
+
+struct Assignment2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
